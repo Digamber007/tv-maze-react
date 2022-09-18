a232ce16557f4d6c127d5c05aa66988deb2395df
Digamber007 <digambertoppo007@gmail.com>
2022-09-18 17:26:24 +0530

    Tv-Maze-Search-Application

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+// import "../styles.css";
+
+function NavBar() {
+  return (
+    <>
+      <section>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <h1 className="logo">Tv-Maze</h1>
+            <div className="container">
+              <h1 className="header">TV-Maze Search</h1>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+}
+
+export default NavBar;
